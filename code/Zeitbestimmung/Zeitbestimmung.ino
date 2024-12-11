c7f5471e4673d87de6219359425360ef27e7e72c
Benedikt Schnörr <benediktschnoerr@mail.de>
2024-12-11 12:07:12 +0100

    Add function for getting waiting time in full program v1.

---
+++ code/Zeitbestimmung/Zeitbestimmung.ino
@@ -0,0 +1,290 @@
+#include <TimeLib.h>
+
+
+
+
+
+
+// hier soll ich für benedikt die zu wartende Zeit herausfinden
+// es muss auch der Zustand einer Flag abgefragt werden,
+// mit der man abfragen kann ob ein befehl gesendet wurde
+
+// für den nächsten Befehl bekomme ich einen Index
+
+
+/*
+  struct NextCommand {
+  int day;
+  // das bekomme ich von Benedikt zurückk um zu wissen, an welchem Tag der nächste Befehl zu senden ist.
+  int hour;
+  int minute;
+  };
+
+
+  bool neuerBefehl;
+
+
+
+  struct zuWartendeZeit
+  {
+  int Tage;
+  int Stunden;
+  int minute;
+  }
+
+  struct zuWartendeZeit WaitingTime;
+
+
+
+
+  struct zuWartendeZeit WarteZeit(struct NextCommand Next)
+  {
+
+    if(Next.day - weekday() =< 0)
+    {
+      // warte Bis zur Ende der Woche
+      zuWartendeZeit.Tage = -1;
+    }
+    else
+    {
+       zuWartendeZeit.Tage = Next.day - weekday();
+    }
+    if(Next.hour - hour() =< 0)
+    {
+      // warte bis zum nächsten Tag
+      zuWartendeZeit.Stunde = -1;
+    }
+    else {
+      zuWartendeZeit.Stunde = Next.hour - hour();
+    }
+    if(Next.minute - minute() =< 0)
+      // warte bis zur nächsten Vollenstunde
+      zuWartendeZeit.Minute = -1;
+  }
+  else
+  {
+    zuWartendeZeit.Minute = Next.minute - minute();
+  }
+
+  }
+
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct NextCommand {
+  int day;
+  // das bekomme ich von Benedikt zurückk um zu wissen, an welchem Tag der nächste Befehl zu senden ist.
+  int hour;
+  int minute;
+};
+
+/*
+  enum bool{
+  FALSE,
+  TRUE
+  } bool;
+  // C kennt keinen Bool
+*/
+
+bool FlagNeuerBefehl = true;
+
+
+
+typedef struct zuWartendeZeit
+{
+  int Tage;
+  int Stunden;
+  int minute;
+  long inMilliSekunden;
+} zuWartendeZeit;
+
+struct zuWartendeZeit WaitingTime;
+
+
+
+
+/*
+  struct zuWartendeZeit WarteZeit(struct NextCommand Next)
+  {
+
+    if(Next.day - weekday() =< 0)
+    {
+      // warte Bis zur Ende der Woche
+      zuWartendeZeit.Tage = -1;
+    }
+    else
+    {
+       zuWartendeZeit.Tage = Next.day - weekday();
+    }
+    if(Next.hour - hour() =< 0)
+    {
+      // warte bis zum nächsten Tag
+      zuWartendeZeit.Stunde = -1;
+    }
+    else {
+      zuWartendeZeit.Stunde = Next.hour - hour();
+    }
+    if(Next.minute - minute() =< 0)
+      // warte bis zur nächsten Vollenstunde
+      zuWartendeZeit.Minute = -1;
+  }
+  else
+  {
+    zuWartendeZeit.Minute = Next.minute - minute();
+  }
+
+  }
+
+*/
+
+struct NextCommand NB = {3, 9, 30};
+
+// mit 4 13:25
+// kommt raus 0 2 59
+
+// Ich bekomme den nachsten Zeitpubkt und möchte die Wartezeit in ms zurückgeben
+
+unsigned long ErmittelwarteZeit(NextCommand Nachsterbefehl) {
+
+  bool carryMinute = false;
+  bool carryStunde = false;
+  // Carry resetten
+  if (Nachsterbefehl.minute - minute() < 0 )
+  {
+    // Funktioniert
+    WaitingTime.minute =  60 + (Nachsterbefehl.minute - minute());
+    carryMinute = true;
+    Serial.println("Minuten bei gesetztem Carry");
+    Serial.println(WaitingTime.minute);
+
+  }
+  else
+  {
+    WaitingTime.minute =  Nachsterbefehl.minute - minute();
+    carryMinute = false;
+    Serial.println("Minuten bei nicht gesetztem Carry");
+    Serial.println(WaitingTime.minute);
+  }
+
+  if (carryMinute == true)
+  {
+    // eine Stunde kürzer
+    if ((Nachsterbefehl.hour - 1) - hour() < 0)
+    {
+      WaitingTime.Stunden = 24 + (Nachsterbefehl.hour - 1) - hour();
+      carryStunde = true; // Ein Tag weniger
+      Serial.println("Stunden bei gesetzem Carry");
+      Serial.println(WaitingTime.Stunden);
+    }
+    else
+    {
+      WaitingTime.Stunden = (Nachsterbefehl.hour - 1) - hour();
+      carryStunde = false;
+      Serial.println("Positive Stunden bei Carry");
+      Serial.println(WaitingTime.Stunden);
+    }
+  }
+
+  else { // Keine Stunde weniger weil kein Carry
+    if (Nachsterbefehl.hour - hour() < 0)
+    {
+      WaitingTime.Stunden = 24 + (Nachsterbefehl.hour - 1) - hour();
+      carryStunde = true; // Ein Tag weniger
+      Serial.println("Negative Stunden bei nicht gesetztem Carry");
+      Serial.println(WaitingTime.Stunden);
+    }
+    else
+    {
+      WaitingTime.Stunden = Nachsterbefehl.hour - hour();
+      Serial.println("Postive Stunden bei nicht gesetztem Carry");
+      Serial.println(WaitingTime.Stunden);
+    }
+  }
+
+
+  if (carryStunde == true) {
+    // Tag weniger
+    if ((Nachsterbefehl.day - 1) - weekday() < 0)
+    {
+      WaitingTime.Tage = 7 + (Nachsterbefehl.day - 1 - weekday());
+      Serial.println("Negative Tage wenn carry gesetzt");
+      Serial.println(WaitingTime.Tage);
+    }
+    else
+    {
+      WaitingTime.Tage = (Nachsterbefehl.day - 1) - weekday();
+      Serial.println("Positive Tage, wenn carry gesetzt");
+      Serial.println(WaitingTime.Tage);
+
+    }
+  }
+
+  else {
+    if (Nachsterbefehl.day - weekday() < 0)
+    {
+      WaitingTime.Tage = 7 + Nachsterbefehl.day - weekday();
+      Serial.println(" negative Tage, wenn carry nicht gesetzt");
+      Serial.println(WaitingTime.Tage);
+    }
+    else
+    {
+      WaitingTime.Tage = Nachsterbefehl.day - weekday();
+      Serial.println("Positive Tage, wenn carry nicht gesetzt");
+      Serial.println(WaitingTime.Tage);
+    }
+  }
+
+
+
+  // Berechnung der Zeit in ms
+
+  WaitingTime.inMilliSekunden = WaitingTime.minute * 60 * 1000 + WaitingTime.Stunden * 60 * 60 * 1000 + WaitingTime.Tage * 60 * 60 * 24 * 1000 ;
+
+
+  return WaitingTime.inMilliSekunden;
+};
+
+
+
+int compileHour, compileMinute, compileSecond, compileYear, compileDay;
+char compileMonth[4];
+
+
+int monthToInt(const char* month) {
+  // Array der Monatsnamen
+  const char* months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+  // Durchlaufen des Arrays und Vergleich der Monatsnamen
+  for (int i = 0; i < 12; ++i) {
+    if (strncmp(month, months[i], 3) == 0) {
+      return i + 1; // Monat gefunden, Index zurückgeben
+    }
+  } // Falls der Monat nicht gefunden wird, -1 zurückgeben
+  return -1;
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Hallo");
+
+
+  // setup time library
+  int compileHour, compileMinute, compileSecond, compileYear, compileDay;
+  char compileMonth[4];
+  sscanf(__TIME__, "%d:%d:%d", &compileHour, &compileMinute, &compileSecond);
+  sscanf(__DATE__, "%s %d %d", &compileMonth, &compileDay, &compileYear);
+  setTime(compileHour, compileMinute, compileSecond, compileDay, monthToInt(compileMonth), compileYear);
+  Serial.println(weekday());
+  Serial.println(hour());
+  unsigned long WarteZeit = ErmittelwarteZeit(NB);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  unsigned long WarteZeit = ErmittelwarteZeit(NB);
+  delay(10000);
+}
--- code/ir_esp_full_v1/ir_esp_full_v1.ino
+++ code/ir_esp_full_v1/ir_esp_full_v1.ino
@@ -1,19 +1,22 @@
 #include <Arduino.h>
 #include <IRremote.hpp> // include the library
 #include <TimeLib.h>
-#include <WiFi.h>
-#include <ESPAsyncWebSrv.h>
-#include <SPIFFS.h>
+#include "WiFi.h"
+#include "ESPAsyncWebSrv.h"
+#include "SPIFFS.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 const char* ssid = "iPhone von Benedikt (2)"; // CHANGE IT
 const char* password = "bene12345"; // CHANGE IT
 
 /*
-const char* ssid = "WG-LAN";
-const char* password = "FredTheFridge!RIP";
+  const char* ssid = "WG-LAN";
+  const char* password = "FredTheFridge!RIP";
 */
 
-
+AsyncWebServer server(80);
 
 int compileHour, compileMinute, compileSecond, compileYear, compileDay;
 char compileMonth[4];
@@ -23,7 +26,8 @@ bool newCommand = false;
 unsigned int previousMillis = 0;
 
 struct CommandParam {
-  int time[2];
+  int hours;
+  int minutes;
   int weekdays[7]; // with sunday at index 0
 };
 
@@ -34,65 +38,75 @@ struct NextCommand {
   int minute;
 };
 
-struct CommandParam paramOn = {{10, 30}, {0, 0, 1, 1, 1, 0, 0}};
-struct CommandParam paramOff = {{16, 30}, {0, 0, 1, 1, 1, 0, 0}};
-struct CommandParam paramPlaceholder = {{0, 0}, {0, 0, 0, 0, 0, 0, 0}};
+struct CommandParam paramOn = {11, 53, {0, 0, 1, 1, 1, 0, 0}};
+struct CommandParam paramOff = {22, 30, {0, 0, 1, 1, 1, 0, 0}};
+struct CommandParam paramPlaceholder = {0, 0, {0, 0, 0, 0, 0, 0, 0}};
+
+typedef struct zuWartendeZeit
+{
+  int Tage;
+  int Stunden;
+  int minute;
+  long inMilliSekunden;
+}zuWartendeZeit;
+
+struct zuWartendeZeit WaitingTime;
 
 void setupNetwork(void) {
   // WiFi.scanNetworks will return the number of networks found.
   int n = WiFi.scanNetworks();
   Serial.println("Scan done");
   if (n == 0) {
-      Serial.println("no networks found");
+    Serial.println("no networks found");
   } else {
-      Serial.print(n);
-      Serial.println(" networks found");
-      Serial.println("Nr | SSID                             | RSSI | CH | Encryption");
-      for (int i = 0; i < n; ++i) {
-          // Print SSID and RSSI for each network found
-          Serial.printf("%2d",i + 1);
-          Serial.print(" | ");
-          Serial.printf("%-32.32s", WiFi.SSID(i).c_str());
-          Serial.print(" | ");
-          Serial.printf("%4d", WiFi.RSSI(i));
-          Serial.print(" | ");
-          Serial.printf("%2d", WiFi.channel(i));
-          Serial.print(" | ");
-          switch (WiFi.encryptionType(i))
-          {
-          case WIFI_AUTH_OPEN:
-              Serial.print("open");
-              break;
-          case WIFI_AUTH_WEP:
-              Serial.print("WEP");
-              break;
-          case WIFI_AUTH_WPA_PSK:
-              Serial.print("WPA");
-              break;
-          case WIFI_AUTH_WPA2_PSK:
-              Serial.print("WPA2");
-              break;
-          case WIFI_AUTH_WPA_WPA2_PSK:
-              Serial.print("WPA+WPA2");
-              break;
-          case WIFI_AUTH_WPA2_ENTERPRISE:
-              Serial.print("WPA2-EAP");
-              break;
-          case WIFI_AUTH_WPA3_PSK:
-              Serial.print("WPA3");
-              break;
-          case WIFI_AUTH_WPA2_WPA3_PSK:
-              Serial.print("WPA2+WPA3");
-              break;
-          case WIFI_AUTH_WAPI_PSK:
-              Serial.print("WAPI");
-              break;
-          default:
-              Serial.print("unknown");
-          }
-          Serial.println();
-          delay(10);
+    Serial.print(n);
+    Serial.println(" networks found");
+    Serial.println("Nr | SSID                             | RSSI | CH | Encryption");
+    for (int i = 0; i < n; ++i) {
+      // Print SSID and RSSI for each network found
+      Serial.printf("%2d", i + 1);
+      Serial.print(" | ");
+      Serial.printf("%-32.32s", WiFi.SSID(i).c_str());
+      Serial.print(" | ");
+      Serial.printf("%4d", WiFi.RSSI(i));
+      Serial.print(" | ");
+      Serial.printf("%2d", WiFi.channel(i));
+      Serial.print(" | ");
+      switch (WiFi.encryptionType(i))
+      {
+        case WIFI_AUTH_OPEN:
+          Serial.print("open");
+          break;
+        case WIFI_AUTH_WEP:
+          Serial.print("WEP");
+          break;
+        case WIFI_AUTH_WPA_PSK:
+          Serial.print("WPA");
+          break;
+        case WIFI_AUTH_WPA2_PSK:
+          Serial.print("WPA2");
+          break;
+        case WIFI_AUTH_WPA_WPA2_PSK:
+          Serial.print("WPA+WPA2");
+          break;
+        case WIFI_AUTH_WPA2_ENTERPRISE:
+          Serial.print("WPA2-EAP");
+          break;
+        case WIFI_AUTH_WPA3_PSK:
+          Serial.print("WPA3");
+          break;
+        case WIFI_AUTH_WPA2_WPA3_PSK:
+          Serial.print("WPA2+WPA3");
+          break;
+        case WIFI_AUTH_WAPI_PSK:
+          Serial.print("WAPI");
+          break;
+        default:
+          Serial.print("unknown");
       }
+      Serial.println();
+      delay(10);
+    }
   }
   Serial.println("");
 
@@ -120,7 +134,7 @@ void setup() {
   // setup ir library
   pinMode(22, OUTPUT);
   IrSender.begin(22);
-  
+
   // setup time library
   sscanf(__TIME__, "%d:%d:%d", &compileHour, &compileMinute, &compileSecond);
   sscanf(__DATE__, "%s %d %d", &compileMonth, &compileDay, &compileYear);
@@ -130,36 +144,36 @@ void setup() {
   setupNetwork();
 
   // initialize SPIFFS
-  if(!SPIFFS.begin(true)){
+  if (!SPIFFS.begin(true)) {
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
 
   // define routes to serve html page and get parameters
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
     Serial.println("ESP32 Web Server: New request received:");  // for debugging
     Serial.println("GET /");        // for debugging
     request->send(SPIFFS, "/index.html", "text/html");
   });
-  server.on("/setOn", HTTP_GET, [](AsyncWebServerRequest *request){
-    paramOn.time = request->getParam("time")->value();
-    paramOn.weekdays = request->getParam("week")->value();
+  server.on("/setOn", HTTP_GET, [](AsyncWebServerRequest * request) {
+    //paramOn.time = request->getParam("time")->value();
+    //paramOn.weekdays = request->getParam("week")->value();
     newCommand = true;
-    Serial.print("on request set: " + paramOn.time + ", " + paramOn.weekdays);
+    //Serial.print("on request set: " + String(paramOn.time) + ", " + String(paramOn.weekdays));
     request->send(SPIFFS, "/index.html", "text/html");
   });
-  server.on("/setOff", HTTP_GET, [](AsyncWebServerRequest *request){
-    paramOff.time = request->getParam("time")->value();
-    paramOff.weekdays = request->getParam("week")->value();
+  server.on("/setOff", HTTP_GET, [](AsyncWebServerRequest * request) {
+    //paramOff.time = request->getParam("time")->value();
+    //paramOff.weekdays = request->getParam("week")->value();
     newCommand = true;
-    Serial.print("off request set: " + paramOff.time + ", " + paramOff.weekdays);
+    //Serial.print("off request set: " + String(paramOff.time) + ", " + String(paramOff.weekdays));
     request->send(SPIFFS, "/index.html", "text/html");
   });
-  server.on("/setPlaceholder", HTTP_GET, [](AsyncWebServerRequest *request){
-    paramPlaceholder.time = request->getParam("time")->value();
-    paramPlaceholder.weekdays = request->getParam("week")->value();
+  server.on("/setPlaceholder", HTTP_GET, [](AsyncWebServerRequest * request) {
+    //paramPlaceholder.time = request->getParam("time")->value();
+    //paramPlaceholder.weekdays = request->getParam("week")->value();
     newCommand = true;
-    Serial.print("placeholder request set: " + paramPlaceholder.time + ", " + paramPlaceholder.weekdays);
+    //Serial.print("placeholder request set: " + String(paramPlaceholder.time) + ", " + String(paramPlaceholder.weekdays));
     request->send(SPIFFS, "/index.html", "text/html");
   });
 
@@ -185,20 +199,21 @@ void loop() {
   newCommand = false;
 
   // get waiting time
-  unsigned long waitInterval = getWaitingTime(next);
+  //unsigned long waitInterval = getWaitingTime(next);
+  unsigned long waitInterval = ErmittelwarteZeit(next);
   Serial.println("wait for " + String(waitInterval));
-  
+
   // wait
   previousMillis = millis();
   unsigned long currentMillis = millis();
-  while(true) {
+  while (true) {
     // check if new parameters are received
     if (newCommand) {
       break;
     }
     // check if waiting time is reached
     currentMillis = millis();
-    float currentWaitTimeTmp = (currentMillis - previousMillis)/1000;
+    float currentWaitTimeTmp = (currentMillis - previousMillis) / 1000;
     Serial.println("waited for " + String(currentWaitTimeTmp));
     if (currentMillis - previousMillis >= waitInterval) {
       sendNec(next);
@@ -207,46 +222,54 @@ void loop() {
   }
 
   Serial.println("finished sending command");
-  delay(19000);
+  delay(20000);
 }
 
-void sendNec(void) {
+void sendNec(const NextCommand& next) {
   Serial.println("send nec command");
-  IrSender.sendNECRaw(0x46228248, sRepeats);
+  IrSender.sendNECRaw(0x46228248, 0);
   delay(1000);
-  IrSender.sendNECRaw(0x46228248, sRepeats);
+  IrSender.sendNECRaw(0x46228248, 0);
   delay(1000);
-  IrSender.sendNECRaw(0x46228248, sRepeats);
+  IrSender.sendNECRaw(0x46228248, 0);
 }
 
-NextCommand getNextCommandStruct(const CommandParam& paramCmd) {
-  struct NextCommand nextCmd = {-1, -1, -1};
+NextCommand getNextCommandStruct(const CommandParam paramCmd) {
+  Serial.println("-----");
+  Serial.println("get next command");
+  Serial.println("params: " + String(paramCmd.hours) + ":" + String(paramCmd.minutes));
+  struct NextCommand nextCmd = { -1, -1, -1};
   // get current weekday
   int today = weekday() - 1;
   int searchDay = today;
   while (1) {
     // check if command should be sent today with current weekday as index for weekdays array
     if (paramCmd.weekdays[searchDay] == 1) {
+      Serial.println("send next command on id " + String(searchDay));
       if (searchDay == today) {
+        Serial.println("send next command today");
         // check if current time is later than next command time
         String currentTime = String(hour());
-        if (minute() < 10) {
-          currentTime += "0" + String(minute());
-        }else {
-          currentTime += String(minute());
+        int minut = minute();
+        if (minut < 10) {
+          currentTime += "0" + String(minut);
+        } else {
+          currentTime += String(minut);
         }
         int currentTimeInt = currentTime.toInt();
-        String cmdTime = String(paramCmd.time[0]);
-        if (paramCmd.time[1] < 10) {
-          cmdTime += "0" + String(paramCmd.time[1]);
-        }else {
-          cmdTime += String(paramCmd.time[1]);
+        String cmdTime = String(paramCmd.hours);        
+        if (paramCmd.minutes < 10) {
+          cmdTime += "0" + String(paramCmd.minutes);
+        } else {
+          cmdTime += String(paramCmd.minutes);
         }
         int cmdTimeInt = cmdTime.toInt();
         if (cmdTimeInt > currentTimeInt) {
+          Serial.println("send time is later today");
           break;
         }
-      }else{
+      } else {
+        Serial.println("send next command at some point");
         break;
       }
     }
@@ -254,6 +277,7 @@ NextCommand getNextCommandStruct(const CommandParam& paramCmd) {
     searchDay += 1;
     if (searchDay > 6) {
       searchDay = 0;
+      Serial.println("go to sunday");
     }
     // break when current weekday is reached again (no days found)
     if (searchDay == today) {
@@ -263,9 +287,9 @@ NextCommand getNextCommandStruct(const CommandParam& paramCmd) {
       return nextCmd;
     }
   }
-  nextCmd.day = searchDay;
-  nextCmd.hour = paramOn.time[0];
-  nextCmd.minute = paramOn.time[1];
+  nextCmd.day = searchDay + 1;
+  nextCmd.hour = paramCmd.hours;
+  nextCmd.minute = paramCmd.minutes;
   return nextCmd;
 }
 
@@ -294,17 +318,122 @@ NextCommand getNextCmd(void) {
   Serial.println("start getting next cmd");
   struct NextCommand nextOnCmd = getNextCommandStruct(paramOn);
   nextOnCmd.command = 0x46A88248;
+  Serial.println("next on cmd: " + String(nextOnCmd.day) + ", " + String(nextOnCmd.hour) + ":" + String(nextOnCmd.minute));
+  Serial.println("-----");
+  Serial.println("paramOff: " + String(paramOff.hours) + ":" + String(paramOff.minutes));
   struct NextCommand nextOffCmd = getNextCommandStruct(paramOff);
   nextOffCmd.command = 0x46228248;
+  Serial.println("next off cmd: " + String(nextOffCmd.day) + ", " + String(nextOffCmd.hour) + ":" + String(nextOffCmd.minute));
   struct NextCommand nextPlaceholderCmd = getNextCommandStruct(paramPlaceholder);
   nextPlaceholderCmd.command = 0x00000000;
+  Serial.println("next 3rd cmd: " + String(nextPlaceholderCmd.day) + ", " + String(nextPlaceholderCmd.hour) + ":" + String(nextPlaceholderCmd.minute));
 
   struct NextCommand earliest = getEarliest(nextOnCmd, nextOffCmd, nextPlaceholderCmd);
-  Serial.println("next command: " + + String(earliest.command) + " @ " + String(earliest.day) + ", " + String(earliest.hour) + ":" + String(earliest.minute));
+  Serial.println("next command: " + String(earliest.command) + " @ " + String(earliest.day) + ", " + String(earliest.hour) + ":" + String(earliest.minute));
   return earliest;
 }
 
-unsigned long getWaitingTime(const NextCommand& next) {
-  unsigned long time = 10000;
-  return time;
-}
+//unsigned long getWaitingTime(const NextCommand& next) {
+//  unsigned long time = 10000;
+//  return time;
+//}
+
+unsigned long ErmittelwarteZeit(NextCommand Nachsterbefehl) {
+
+  bool carryMinute = false;
+  bool carryStunde = false;
+  // Carry resetten
+  if (Nachsterbefehl.minute - minute() < 0 )
+  {
+    // Funktioniert
+    WaitingTime.minute =  60 + (Nachsterbefehl.minute - minute());
+    carryMinute = true;
+    Serial.println("Minuten bei gesetztem Carry");
+    Serial.println(WaitingTime.minute);
+
+  }
+  else
+  {
+    WaitingTime.minute =  Nachsterbefehl.minute - minute();
+    carryMinute = false;
+    Serial.println("Minuten bei nicht gesetztem Carry");
+    Serial.println(WaitingTime.minute);
+  }
+
+  if (carryMinute == true)
+  {
+    // eine Stunde kürzer
+    if ((Nachsterbefehl.hour - 1) - hour() < 0)
+    {
+      WaitingTime.Stunden = 24 + (Nachsterbefehl.hour - 1) - hour();
+      carryStunde = true; // Ein Tag weniger
+      Serial.println("Stunden bei gesetzem Carry");
+      Serial.println(WaitingTime.Stunden);
+    }
+    else
+    {
+      WaitingTime.Stunden = (Nachsterbefehl.hour - 1) - hour();
+      carryStunde = false;
+      Serial.println("Positive Stunden bei Carry");
+      Serial.println(WaitingTime.Stunden);
+    }
+  }
+
+  else { // Keine Stunde weniger weil kein Carry
+    if (Nachsterbefehl.hour - hour() < 0)
+    {
+      WaitingTime.Stunden = 24 + (Nachsterbefehl.hour - 1) - hour();
+      carryStunde = true; // Ein Tag weniger
+      Serial.println("Negative Stunden bei nicht gesetztem Carry");
+      Serial.println(WaitingTime.Stunden);
+    }
+    else
+    {
+      WaitingTime.Stunden = Nachsterbefehl.hour - hour();
+      Serial.println("Postive Stunden bei nicht gesetztem Carry");
+      Serial.println(WaitingTime.Stunden);
+    }
+  }
+
+
+  if (carryStunde == true) {
+    // Tag weniger
+    if ((Nachsterbefehl.day - 1) - weekday() < 0)
+    {
+      WaitingTime.Tage = 7 + (Nachsterbefehl.day - 1 - weekday());
+      Serial.println("Negative Tage wenn carry gesetzt");
+      Serial.println(WaitingTime.Tage);
+    }
+    else
+    {
+      WaitingTime.Tage = (Nachsterbefehl.day - 1) - weekday();
+      Serial.println("Positive Tage, wenn carry gesetzt");
+      Serial.println(WaitingTime.Tage);
+
+    }
+  }
+
+  else {
+    if (Nachsterbefehl.day - weekday() < 0)
+    {
+      WaitingTime.Tage = 7 + Nachsterbefehl.day - weekday();
+      Serial.println(" negative Tage, wenn carry nicht gesetzt");
+      Serial.println(WaitingTime.Tage);
+    }
+    else
+    {
+      WaitingTime.Tage = Nachsterbefehl.day - weekday();
+      Serial.println("Positive Tage, wenn carry nicht gesetzt");
+      Serial.println(WaitingTime.Tage);
+    }
+  }
+
+
+
+  // Berechnung der Zeit in ms
+
+  WaitingTime.inMilliSekunden = WaitingTime.minute * 60 * 1000 + WaitingTime.Stunden * 60 * 60 * 1000 + WaitingTime.Tage * 60 * 60 * 24 * 1000 ;
+
+
+  return WaitingTime.inMilliSekunden;
+};
